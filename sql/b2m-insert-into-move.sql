91bd8283324bd1460d0c2820e57060831f17e6b0
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-15 05:52:51 -0500

    Accidentally overwrote correct sql query with a different one. Should now be the correct query again.

--- sql/b2m-insert-into-move.sql
+++ sql/b2m-insert-into-move.sql
@@ -1,1 +1,5 @@
-﻿SELECT * FROM move
+﻿INSERT INTO move (name, description, sweatRate)
+VALUES
+('Push up', 'Ga horizontaal liggen op teentoppen en handen. Laat het lijf langzaam zakken tot de neus de grond bijna raakt. Duw het lijf terug nu omhoog tot de ellebogen bijna gestrekt zijn. Vervolgens weer laten zakken. Doe dit 20 keer zonder tussenpauzes', 3),
+('Planking', 'Ga horizontaal liggen op teentoppen en onderarmen. Houdt deze positie 1 minuut vast', 3),
+('Squat', 'Ga staan met gestrekte armen. Zak door de knieën tot de billen de grond bijna raken. Ga weer volledig gestrekt staan. Herhaal dit 20 keer zonder tussenpauzes', 5);
