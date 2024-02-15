e6a3a67d6b1946ef89b94c8e7d765f1db2c6554c
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-15 03:56:05 -0500

    #3 - initial commit for Move class

---
+++ BornToMove/Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BornToMove {
+    public class Move {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int sweatRate { get; set; }
+        public Move(int id, string name, string description, int sweatRate) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.sweatRate = sweatRate;
+        }
+
+        public void ShowMove() {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Sweat Rate: " + sweatRate);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+}
