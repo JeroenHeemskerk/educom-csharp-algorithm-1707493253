119f0c630205cc08496117b4799a3881bd65da53
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-19 11:11:07 -0500

    #5 - Updated BuMove class, no longer static. Added rating functions.

--- BornToMove.Business/BuMove.cs
+++ BornToMove.Business/BuMove.cs
@@ -1,34 +1,62 @@
 ﻿using BornToMove.DAL;
 using Microsoft.Data.SqlClient;
+using System;
 using System.Data;
+using System.Security.Cryptography;
 
 namespace BornToMove.Business {
-    public static class BuMove {
+    public class BuMove {
+        private readonly MoveCrud moveCrud;
 
-        public static Move GetRandomMove() {
-            List<Move> moves = MoveCrud.ReadAllMoves();
+        public BuMove(MoveCrud moveCrud) {
+            this.moveCrud = moveCrud;
+        }
+
+        public Move GetRandomMove() {
+            List<Move> moves = moveCrud.ReadAllMoves();
             Random rng = new Random();
             int selectedMoveId = rng.Next(moves.Count);
             Move move = moves[selectedMoveId];
             return move;
         }
 
-        public static List<Move> GetAllMoves() {
-            var x = MoveCrud.ReadAllMoves();
-            Console.WriteLine(x);
-            return x;
+        public List<Move> GetAllMoves() {
+            var moves = moveCrud.ReadAllMoves();
+            return moves;
         }
 
-        public static bool AddMove(Move m) {
+        public bool AddMove(Move m) {
             if(!doesMoveExist(m.name)) {
-                MoveCrud.Create(m);
+                moveCrud.Create(m);
                 return true;
             }
             return false;
         }
 
-        public static bool doesMoveExist(string name) {
-            return MoveCrud.ReadMoveByName(name) != null;
+        public bool doesMoveExist(string name) {
+            return moveCrud.ReadMoveByName(name) != null;
+        }
+
+        public void PopulateMoveTable() {
+            if (moveCrud.ReadAllMoves().Count == 0) {
+                var sql = File.ReadAllText("C:\\Users\\thoma\\Source\\Repos\\educom-csharp-algorithm\\sql\\b2m-insert-into-move.sql");
+                moveCrud.RunSqlCommand(sql);
+            }
+        }
+
+        //----------------------------------------------------------
+        //-------------------- RATING FUNCTIONS --------------------
+        //----------------------------------------------------------
+
+        public void AddRating(MoveRating r) {
+            r.Move.Ratings.Add(r);
+            moveCrud.AddRating(r);
+            moveCrud.Update(r.Move);
+        }
+
+        public double GetAverageRating(Move move) {
+            move.Ratings = moveCrud.ReadAllRatingsByMove(move);
+            return move.Ratings.Average(r => r.Rating);
         }
     }
 }
