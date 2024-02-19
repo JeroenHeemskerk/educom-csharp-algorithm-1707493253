55a94a0a3d3e6f9327953e53f7b12793a30bff7e
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-19 11:08:00 -0500

    #5 - Move class updated to have a rating list. Average rating is printed in showMove function

--- BornToMove.DAL/Move.cs
+++ BornToMove.DAL/Move.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BornToMove.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,22 +11,32 @@ namespace BornToMove {
         public string name { get; set; }
         public string description { get; set; }
         public int sweatRate { get; set; }
+        virtual public ICollection<MoveRating> Ratings { get; set; }
         public Move(int id, string name, string description, int sweatRate) {
             this.id = id;
             this.name = name;
             this.description = description;
             this.sweatRate = sweatRate;
+            this.Ratings = RatingCrud.ReadAllRatingsByMove(this);
         }
 
         public Move(string name, string description, int sweatRate) {
             this.name = name;
             this.description = description;
             this.sweatRate = sweatRate;
+            this.Ratings = RatingCrud.ReadAllRatingsByMove(this);
+        }
+
+        public Move() {
+            name = "";
+            description = "";
+            Ratings = new List<MoveRating>();
         }
 
         public void ShowMove() {
             Console.WriteLine("Name:        " + name);
             Console.WriteLine("Sweat Rate:  " + sweatRate);
+            Console.WriteLine("Rating:      " + Math.Round(Ratings.Select(r => r.Rating).DefaultIfEmpty().Average(), 2) + "/5");
             Console.WriteLine("Description: " + description);
         }
     }
