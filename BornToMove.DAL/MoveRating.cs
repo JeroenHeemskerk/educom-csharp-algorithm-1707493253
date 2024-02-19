ece1856c0bebb63cb6a0f769ec1b9253234ccf62
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-19 11:09:36 -0500

    #5 - initial commit for MoveRating class

---
+++ BornToMove.DAL/MoveRating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BornToMove.DAL {
+    public class MoveRating {
+        public int id { get; set; }
+        public Move Move {  get; set; }
+        public double Rating { get; set; }
+        public double Vote { get; set; }
+        public MoveRating(double rating, double vote) {
+            Move = new Move();
+            Rating = rating;
+            Vote = vote;
+        }
+    }
+}
