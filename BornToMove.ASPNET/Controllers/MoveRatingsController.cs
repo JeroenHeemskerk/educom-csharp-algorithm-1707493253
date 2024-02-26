5fdf8f977e0f93fe580f34ce97eb9c6bdf754896
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-02-26 05:43:22 -0500

    Added controllers for ASP.NET program

---
+++ BornToMove.ASPNET/Controllers/MoveRatingsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BornToMove.DAL;
+
+namespace BornToMove.ASPNET.Controllers
+{
+    public class MoveRatingsController : Controller
+    {
+        private readonly MoveContext _context;
+
+        public MoveRatingsController(MoveContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MoveRatings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MoveRating.ToListAsync());
+        }
+
+        // GET: MoveRatings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moveRating = await _context.MoveRating
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (moveRating == null)
+            {
+                return NotFound();
+            }
+
+            return View(moveRating);
+        }
+
+        // GET: MoveRatings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MoveRatings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Rating,Vote")] MoveRating moveRating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(moveRating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(moveRating);
+        }
+
+        // GET: MoveRatings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moveRating = await _context.MoveRating.FindAsync(id);
+            if (moveRating == null)
+            {
+                return NotFound();
+            }
+            return View(moveRating);
+        }
+
+        // POST: MoveRatings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Rating,Vote")] MoveRating moveRating)
+        {
+            if (id != moveRating.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(moveRating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MoveRatingExists(moveRating.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(moveRating);
+        }
+
+        // GET: MoveRatings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moveRating = await _context.MoveRating
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (moveRating == null)
+            {
+                return NotFound();
+            }
+
+            return View(moveRating);
+        }
+
+        // POST: MoveRatings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var moveRating = await _context.MoveRating.FindAsync(id);
+            if (moveRating != null)
+            {
+                _context.MoveRating.Remove(moveRating);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MoveRatingExists(int id)
+        {
+            return _context.MoveRating.Any(e => e.id == id);
+        }
+    }
+}
--- BornToMove.ASPNET/Controllers/MovesController.cs
+++ BornToMove.ASPNET/Controllers/MovesController.cs
@@ -1,7 +1,7 @@
-﻿using BornToMove.Business;
+﻿using BornToMove.ASPNET.Models;
+using BornToMove.Business;
 using BornToMove.DAL;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace BornToMove.ASPNET.Controllers
 {
@@ -15,6 +15,7 @@ namespace BornToMove.ASPNET.Controllers
         public IActionResult Index()
         {
             List<MoveRating> moves = _buMove.GetAllMoves();// code om de lijst met moves op te halen inclusief de gemiddelde rating
+            ViewBag.Moves = moves;
             return View(moves);
         }
         public IActionResult Details(int id)
@@ -23,11 +24,76 @@ namespace BornToMove.ASPNET.Controllers
             return View(move);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details([FromForm] MoveRating m) {
+            if (ModelState.IsValid) {
+                m.Move = _buMove.GetMoveById(m.Move.id)?.Move ?? m.Move;
+                _buMove.AddRating(m);
+                return RedirectToAction(nameof(Index));
+            }
+            m = _buMove.GetMoveById(m.Move.id) ?? m;
+            return View(m);
+        }
+
+        public IActionResult Edit(int id) {
+            Move? move = _buMove.GetMoveById(id)?.Move;
+            return View(move);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,[FromForm] Move updatedMove) {
+            if (updatedMove.id != id) {
+                ModelState.AddModelError("id", "Bad Request");
+            }
+            bool other = _buMove.doesMoveExist(updatedMove.name) && _buMove.GetMoveByName(updatedMove.name)?.id != id;
+            if (other) {
+                ModelState.AddModelError("name", "This move name is already being used.");
+            }
+            if (ModelState.IsValid) {
+                _buMove.EditMove(updatedMove);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(updatedMove);
+        }
+
+        public IActionResult Delete(int id) {
+            Move? move = _buMove.GetMoveById(id)?.Move;
+            return View(move);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, [FromForm] Move m) {
+            if (m.id != id) {
+                ModelState.AddModelError("id", "Bad Request");
+                return View(m);
+            }
+            _buMove.DeleteMove(m);
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Random()
         {
-            Move randomMove = _buMove.GetRandomMove(); // code om een random move op te halen
+            MoveRating randomMove = _buMove.GetRandomMove(); // code om een random move op te halen
+            return View("Details", randomMove);
+        }
 
-            return View("Details", new MoveRating(0, 0) { Move = randomMove });
+        public IActionResult CreateMove() {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateMove(Move m) {
+            if(_buMove.doesMoveExist(m.name)) {
+                ModelState.AddModelError("name", "This move name is already being used.");
+            }
+            if (ModelState.IsValid) {
+                _buMove.AddMove(m);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(m);
         }
     }
 }
